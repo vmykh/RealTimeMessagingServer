03ba0d34c21325e3a10ecde0272c3141fb381e3a
Vitaly Mykhalko <mikhalko.ukr@gmail.com>
2016-01-10 01:15:58 +0200

    initialize testing environment
    create gradle build script and configure environment for unit tests and acceptance tests

---
+++ src/main/java/dev/vmykh/rtmessaging/Main.java
@@ -0,0 +1,4 @@
+package dev.vmykh.rtmessaging;
+
+public class Main {
+}
---
+++ src/test/java/dev/vmykh/rtmessaging/UnitTestStub.java
@@ -0,0 +1,17 @@
+package dev.vmykh.rtmessaging;
+
+import org.junit.Test;
+
+public class UnitTestStub {
+
+	@Test
+	public void unitTestStub1() {
+		System.out.println("Unit test stub one!1!!!!!!!11150q");
+	}
+
+
+	@Test
+	public void unitTestStub2() {
+		System.out.println("Unit test stub two!!11!!111!1q");
+	}
+}
